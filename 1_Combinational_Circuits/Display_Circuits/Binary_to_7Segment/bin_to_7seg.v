2911231c12d69b20d51dd792db71a1f0d5f654c4
rijj18 <rsharma6_be24@thapar.edu>
2026-03-24 21:30:04 +0530

    Create bin_to_7seg.v

---
+++ 1_Combinational_Circuits/Display_Circuits/Binary_to_7Segment/bin_to_7seg.v
@@ -0,0 +1,22 @@
+module bin_7seg(
+    input [3:0] bin,
+    output reg [6:0] sout
+    );
+    always@(*)
+    begin
+    case(bin)
+    4'b0000:sout=7'b1111110;
+    4'b0001:sout=7'b0110000;
+    4'b0010:sout=7'b1101101;
+    4'b0011:sout=7'b1111001;
+    4'b0100:sout=7'b0110011;
+    4'b0101:sout=7'b1011011;
+    4'b0110:sout=7'b1011111;
+    4'b0111:sout=7'b1110000;
+    4'b1000:sout=7'b1111111;
+    4'b1001:sout=7'b1111011;
+    default:sout=7'b0000000;
+    endcase 
+    end 
+    
+endmodule
