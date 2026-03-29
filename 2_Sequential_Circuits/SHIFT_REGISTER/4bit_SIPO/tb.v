5d0e1b2b1f01819d435cd2f228599dbc2768e853
rijj18 <rsharma6_be24@thapar.edu>
2026-03-30 00:33:52 +0530

    enhanced testbench

--- 2_Sequential_Circuits/SHIFT_REGISTER/4bit_SIPO/tb.v
+++ 2_Sequential_Circuits/SHIFT_REGISTER/4bit_SIPO/tb.v
@@ -4,7 +4,7 @@ reg clk, rst, sin;
   siso uut (
     .clk(clk),
     .rst(rst),
-    .si(sin),
+    .sin(sin),
     .q(q)
 );
 always #5 clk = ~clk;
@@ -17,10 +17,10 @@ initial begin
     #10 rst = 0;
 
     // Apply serial input
-    #10 si = 1;
-    #10 si = 0;
-    #10 si = 1;
-    #10 si = 1;
+    #10 sin = 1;
+    #10 sin = 0;
+    #10 sin = 1;
+    #10 sin= 1;
 
     #50 $finish;
 end
