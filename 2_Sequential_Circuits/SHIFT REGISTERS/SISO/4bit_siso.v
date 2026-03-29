9a4b1070f5e58615d1605a424d409e2e8aab59f1
rijj18 <rsharma6_be24@thapar.edu>
2026-03-29 16:55:01 +0530

    Implementation 4bit_siso.v
    code and improve formatting in 4bit_siso.v.

---
+++ 2_Sequential_Circuits/SHIFT REGISTERS/SISO/4bit_siso.v
@@ -0,0 +1,16 @@
+module(
+  input clk,rst,si,  
+  output so
+);
+  reg [3:0] shift_reg;
+  always@(posedge clk or posedge rst)
+    begin
+    if(rst)
+      shift_reg=4'b0000
+      else
+       shift_reg <= {shift_reg[2:0], si};  // shift left & insert input
+        end
+      assign so = shift_reg[3];  // MSB is output
+endmodule
+  
+  
